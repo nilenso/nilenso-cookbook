bc9286a0c75aa046f6271c414cef4f8bc87cf791
Akshay Gupta <kitallis@gmail.com>
2013-09-24 18:38:24 +0530

    adding nginx and un-gitignoring lock files

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,4 @@ description      'Installs/Configures nilenso-cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+depends "nginx", "~> 1.8.0"
