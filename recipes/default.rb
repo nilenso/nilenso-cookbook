22de1f909382fa5f2722863275bb29f3268b1482
Akshay Gupta <kitallis@gmail.com>
2013-09-25 15:32:06 +0530

    refactoring the webserver into a new recipe file

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,31 +6,5 @@ user node[:nilenso][:user] do
   shell "/bin/bash"
 end
 
-include_recipe 'apt'
-include_recipe 'nginx'
-
-nginx_site 'default' do
-  enable false
-end
-
-directory "/var/www/nilenso" do
-  action :create
-  recursive true
-end
-
-template "#{node[:nginx][:dir]}/sites-available/nilenso" do
-  source "nilenso.erb"
-  mode 0777
-  owner node[:nilenso][:user]
-  group node[:nilenso][:group]
-end
-
-nginx_site "nilenso" do
-  enable true
-end
-
-cookbook_file "/var/www/nilenso/index.html" do
-  source "index.html"
-  mode 0755
-  owner node[:nilenso][:user]
-end
+include_recipe "apt"
+include_recipe "nilenso-cookbook::webserver"
